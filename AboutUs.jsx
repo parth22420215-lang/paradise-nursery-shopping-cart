42252c15332d752056582d1f55697806c58c5888
parth22420215-lang <parth.22420215@viit.ac.in>
2026-04-29 02:53:59 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,11 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>Paradise Nursery provides fresh and beautiful indoor plants for homes and offices.</p>
+      <p>We offer quality plants at affordable prices.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
