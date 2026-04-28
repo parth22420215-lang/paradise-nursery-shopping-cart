4f0b00a41d0c1d5fbbaff8907569e7d012eba87c
parth22420215-lang <parth.22420215@viit.ac.in>
2026-04-29 02:57:15 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Paradise Nursery</h1>
+      <h2>Welcome to our Plant Shop</h2>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
